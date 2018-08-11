14a6de3a4b430074c35bcac68da5cff9a8b5d5fe
dima <dp@redstart.pro>
2018-08-11 11:21:34 +0300

    delete all

---
+++ app/src/main/java/ru/yandexschool/hacka2/App.kt
@@ -0,0 +1,11 @@
+package ru.yandexschool.hacka2
+
+import android.app.Application
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/ru/yandexschool/hacka2/main/MainActivity.kt
+++ app/src/main/java/ru/yandexschool/hacka2/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.yandexschool.hacka2
+package ru.yandexschool.hacka2.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ru.yandexschool.hacka2.R
 
 class MainActivity : AppCompatActivity() {
 
